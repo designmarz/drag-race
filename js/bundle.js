087abe645610115137f4b0a57e1d0066eb6d9eac
Nick Marazzo <nick@designmarz.com>
2015-01-23 14:05:59 -0800

    Drag racing game; 2 player done, engine start switch done, html/css 90% done, no animation on the xmas tree, no wins or false start conditions

--- js/bundle.js
+++ js/bundle.js
@@ -1,53 +1,73 @@
-(function() {
-  var ChristmasTree = function() {
-    // this class manages the xmas tree
-    // get a reference to the pre stage lights html element
-    this.$preStageLights = document.getElementById('pre-stage');
-    // reset the christmas tree every time it's initialized
-    this.reset();
-  },
-  RaceTrack         = function() {
-    // this class manages the race track
-  },
-  Dragster          = function() {
+
+// Var to control if the car is on //
+var engine = "off";
+
+// Add keyBinding for movement to Car1
+window.addEventListener('keyup', function(event) {
+	if (event.keyCode === 39 && engine === "on") {
+		console.log("car 1");
+		this.$el = document.getElementById('rCar1');
+		this.$el.style.left = parseInt(this.$el.style.left, 10) + 10 + "px";
+	}
+
+	else if (event.keyCode === 37) {
+		this.$el = document.getElementById('rCar1');
+		this.$el.style.left = parseInt(this.$el.style.left, 10) - 10 + "px";  }
+	});
+// Add keyBinding for movement to Car2
+window.addEventListener('keyup', function(event) {
+	if (event.keyCode === 68 && engine === "on") {
+		console.log("car 2");
+		this.$el = document.getElementById('rCar2');
+		this.$el.style.left = parseInt(this.$el.style.left, 10) + 10 + "px";}
+
+	else if (event.keyCode === 65) {
+		this.$el = document.getElementById('rCar2');
+		this.$el.style.left = parseInt(this.$el.style.left, 10) - 10 + "px";}
+	});
+
+
+
+var carstart = function(){
+
+	if (engine === "off"){
+    // Code for Chrome, Safari, Opera
+    document.getElementById("keySwitch").style.WebkitTransform = "rotate(-30deg)"; 
+    // Code for IE9
+    document.getElementById("keySwitch").style.msTransform = "rotate(-30deg)"; 
+    document.getElementById("keySwitch").style.transform = "rotate(-30deg)"; 
+    engine = "on";
+    console.log("Car 1 On!");
+    return engine;
+}
+
+	if (engine === "on"){
+    // Code for Chrome, Safari, Opera
+    document.getElementById("keySwitch").style.WebkitTransform = "rotate(30deg)"; 
+    // Code for IE9
+    document.getElementById("keySwitch").style.msTransform = "rotate(30deg)"; 
+    document.getElementById("keySwitch").style.transform = "rotate(30deg)"; 
+    engine = "off";
+    console.log("Car 1 Off!");
+    return engine;
+}
+
+
+};
+
+
+
+// Reset position for the individual Cars
+CarMove = function() {
     // grab the car element
-    this.$el = document.getElementById('dragster');
-    // set the starting position of the dragster
+    this.$el = document.getElementById('rCar1');
+    this.$el2 = document.getElementById('rCar2');
+    // set the starting position of the Track
     this.$el.style.left = "0px";
-  },
-  Game              = function() {
-    // this class manages game state
-    // initialize a christmas tree
-    this.tree = new ChristmasTree();
-    // initialize the race track
-    this.track = new RaceTrack();
-    // initialize the player dragster
-    this.dragster = new Dragster();
+    this.$el2.style.left = "0px";
+};
 
-    this.attachListeners();
-  };
 
-  ChristmasTree.prototype.reset = function() {
-    // append the css on class to the pre stage lights
-    // so that they show up as yellow
-    this.$preStageLights.className += " on";
-  };
 
-  Game.prototype.attachListeners = function() {
-    var self = this;
-    // listen for user input, specifically
-    // for the user pressing the right arrow key
-    window.addEventListener('keyup', function(event) {
-      if (event.keyCode === 39) {
-        self.dragster.advance();
-      }
-    });
-  };
-
-  Dragster.prototype.advance = function() {
-    // this should move the car across the screen 1px at a time
-    this.$el.style.left = parseInt(this.$el.style.left, 10) + 1 + "px";
-  };
-
-  var g = new Game();
-})();
+// intialize CarMove
+CarMove();
